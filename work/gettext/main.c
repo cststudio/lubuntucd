14a9c6fe2c2bf93a837370a25c67089fd6a930f9
Late Lee <li@latelee.org>
2018-01-30 13:34:47 +0800

    Add getext test for locale

---
+++ work/gettext/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+#include <locale.h>
+#include <libintl.h>
+
+#define _(str) gettext(str)
+#define LOCALEDIR "/usr/share/locale"
+#define PACKAGE "textdemo"
+
+int main(void)
+{
+    int i = 0;
+    setlocale(LC_ALL, "");
+    bindtextdomain(PACKAGE, LOCALEDIR);
+    textdomain(PACKAGE);
+    
+    printf(_("This is GetText test.\n"));
+    printf(_("Hello world.\n"));
+
+    for (i = 0; i < 3; i++)
+    {
+        printf(_("output: %d\n"), i);
+    }
+
+    printf(_("end of test.\n"));
+    return 0;
+}
